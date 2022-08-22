0f4329a929db249014aeb6f0851abdee4f6d42a1
erickmuriithi <erick.muriithi@student.moringaschool.com>
2022-08-23 01:34:18 +0300

    add file

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+function setBestCustomer() {
+    (bestCustomer = 'not bob');
+}
+console.log(setBestCustomer);
+
+function  overwriteBestCustomer() {
+    (bestCustomer = 'maybe bob');
+}
+console.log(setBestCustomer);
+
+const LeastFavoriteCustomer = 'red flag'
+function changeLeastFavoriteCustomer() {
+    (LeastFavoriteCustomer = 'uncooperative');
+}
+console.log(changeLeastFavoriteCustomer());
